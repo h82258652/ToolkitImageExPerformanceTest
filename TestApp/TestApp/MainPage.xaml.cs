fc7ce14d820547110f623d3430956b70f690a85e
h82258652 <842053625@qq.com>
2020-04-24 10:11:10 +0800

    commit

---
+++ TestApp/TestApp/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml;
+
+namespace TestApp
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OriginalImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(OriginalImagePage));
+        }
+
+        private void ToolkitImageExButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(ToolkitImageExPage));
+        }
+    }
+}
---
+++ TestApp/TestApp/ToolkitImageExPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.Toolkit;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using Windows.System;
+using Windows.UI.Xaml;
+
+namespace TestApp
+{
+    public sealed partial class ToolkitImageExPage
+    {
+        private readonly Stopwatch _stopwatch;
+        private readonly DispatcherTimer _timer;
+
+        public ToolkitImageExPage()
+        {
+            InitializeComponent();
+
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMilliseconds(200)
+            };
+            _timer.Tick += Timer_Tick;
+
+            _stopwatch = new Stopwatch();
+            _stopwatch.Start();
+
+            const string imageUrl = "https://img-prod-cms-rt-microsoft-com.akamaized.net/cms/api/am/imageFileData/RE1Mu3b?ver=5c31";
+            // Simulate load lots of different images.
+            GridView.ItemsSource = Enumerable.Range(0, 500).Select(temp => imageUrl + "&rand=" + Guid.NewGuid()).ToList();
+        }
+
+        private void GoBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack();
+            }
+        }
+
+        private void RefreshMemoryUsage()
+        {
+            var memoryUsage = MemoryManager.AppMemoryUsage;
+            MemoryUsageTextBlock.Text = "Memory usage: " + Converters.ToFileSizeString((long)memoryUsage);
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            RefreshMemoryUsage();
+        }
+
+        private void ToolkitImageExPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            _stopwatch.Stop();
+            LoadCostTextBlock.Text = $"Load cost: {_stopwatch.ElapsedMilliseconds}ms";
+
+            _timer.Start();
+
+            RefreshMemoryUsage();
+        }
+
+        private void ToolkitImageExPage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+        }
+    }
+}
